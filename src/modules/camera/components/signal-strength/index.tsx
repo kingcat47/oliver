1b8f3ee25ff4520accbb69be74762df26b9c15a6
FIRO <72495729+iamfiro@users.noreply.github.com>
2025-10-20 23:28:18 +0900

    feat: enhance Camera components with signal strength metrics and styling updates

--- src/modules/camera/components/signal-strength/index.tsx
+++ src/modules/camera/components/signal-strength/index.tsx
@@ -2,12 +2,46 @@ import { Card } from "@/shared/components";
 
 import s from "./style.module.scss";
 
-interface Props {}
+interface Props {
+  averageDelay: number;
+  maxDelay: number;
+  medianDelay: number;
+}
 
-export default function CameraSignalStrengthCard() {
+export default function CameraSignalStrengthCard({
+  averageDelay,
+  maxDelay,
+  medianDelay,
+}: Props) {
   return (
     <Card>
       <Card.Title>영상 신호 퀄리티</Card.Title>
+      <div className={s.content}>
+        <div>
+          <div className={s.signalStrength}>
+            <span>평균 지연</span>
+            <svg
+              width="10"
+              height="10"
+              viewBox="0 0 10 10"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <circle cx="5" cy="5" r="5" />
+            </svg>
+          </div>
+
+          <span>{averageDelay}ms</span>
+        </div>
+        <div>
+          <span>최대 지연</span>
+          <span>{maxDelay}ms</span>
+        </div>
+        <div>
+          <span>중앙값</span>
+          <span>{medianDelay}ms</span>
+        </div>
+      </div>
     </Card>
   );
 }
--- src/modules/camera/widgets/camera-info-section/index.tsx
+++ src/modules/camera/widgets/camera-info-section/index.tsx
@@ -1,3 +1,4 @@
+import { ArrowUpRight, CircleQuestionMark } from "lucide-react";
 import { useState } from "react";
 
 import { Segment } from "@/shared/components";
@@ -18,7 +19,25 @@ export default function CameraInfoSection() {
         selected={selectedSegment}
         onChange={setSelectedSegment}
       />
-      <h1 className={s.title}>열화상 카메라</h1>
+      <div className={s.content}>
+        <div className={s.header}>
+          <h1 className={s.title}>열화상 카메라</h1>
+          <a
+            href="https://namu.wiki/w/%EC%97%B4%ED%99%94%EC%83%81%EC%B9%B4%EB%A9%94%EB%9D%BC"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <CircleQuestionMark />
+          </a>
+        </div>
+        <p>
+          열화상 카메라는 적외선을 감지해 물체의 온도를 시각적으로 표시하는
+          카메라입니다.
+          <br />
+          즉, 사람이 눈으로 볼 수 없는 열 정보를 영상으로 변환하여, 온도가 높은
+          부분은 밝게, 낮은 부분은 어둡게 표현합니다.
+        </p>
+      </div>
       <CameraSignalStrengthCard />
     </section>
   );
